0f3e84e17c3a82ebfa964eb4c3515eb642eef742
Benedikt Böhm <bb@xnull.de>
2006-08-09 08:49:36 +0000

    move wrappers to own repo from vserver-utils

---
+++ src/procps/vps.c
@@ -0,0 +1,196 @@
+// Copyright 2006 Remo Lemma <coloss7@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include "../wrapper.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <vserver.h>
+#include <getopt.h>
+
+#define PS_BIN "/bin/ps"
+
+int error_mode = 0, fxid = -1;
+
+static const char *rcsid = "$Id: vps.c 315 2006-08-05 21:43:43Z coloss $";
+
+static 
+struct option long_opts[] = {
+	{ "xid", 1, 0, 0x01 },
+	{ "help", 0, 0, 0x02 },
+	{ "version", 0, 0, 0x03 },
+	{ NULL, 0, 0, 0 },
+};
+
+static 
+char *tail_name(char *vname)
+{
+	char *pch;
+	pch = strrchr(vname, '/') + 1;
+	return strdup(pch);
+}
+
+static
+void parse_line(char *line, int n)
+{
+	char *pid_pos, *name = NULL;
+	static int pid_start;
+	pid_t pid;
+	xid_t xid;
+	struct vx_vhi_name vhi_name;
+	
+	if (n == 0) {
+		if ((pid_pos = strstr(line, "  PID")) == 0) {
+			dprintf(STDERR_FILENO, "vps: PID column not found, writing ps output\n");
+			printf(line);
+			error_mode = 1;
+			return;
+		}
+		
+		pid_start = pid_pos - line;
+		
+		printf("  XID NAME     %s\n", line);
+		return;
+	}
+	
+	else {
+		pid = atoi(line + pid_start);
+		
+		if (pid < 0)
+			return;
+		
+		if ((xid = vx_get_task_xid(pid)) == -1)
+			return;
+		
+		if (xid == 0)
+			name = "ADMIN";
+		
+		else if (xid == 1)
+			name = "WATCH";
+		
+		else {
+			vhi_name.field = VHIN_CONTEXT;
+			
+			if (vx_get_vhi_name(xid, &vhi_name) == -1)
+				return;
+			
+			if (vhi_name.name[0] == '/')
+				name = tail_name(vhi_name.name);
+			
+			else
+				name = vhi_name.name;
+		}
+		if (fxid == -1 || xid == fxid)
+			printf("%5d %-8.8s %s\n", xid, name, line);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int p[2], fd, i, status, len, j = 0;
+	pid_t pid;
+	char *line, c, *nargv[argc];
+	
+	/* root access is needed */
+	if (getuid())
+		die("root access is needed");
+	
+	while ((c = getopt_long_only(argc, argv, "", long_opts, NULL)) != -1) {
+		switch (c) {
+			case 0x01:
+				fxid = atoi(optarg);
+				break;
+			case 0x02:
+				printf("Usage: vps <ps options> [--xid <xid>]\n");
+				break;
+			case 0x03:
+				printf("%s\n", rcsid);
+				exit(EXIT_SUCCESS);
+				break;
+			case ':':
+			case '?':
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	argv[0] = PS_BIN;
+
+	if (fxid >= 0) {
+		for (i = 0; i < argc; i++) {
+			if (!strcmp(argv[i], "--xid")) {
+				if (i + 2 < argc)
+					i+=1;
+			} else
+				nargv[j++] = argv[i];
+		}
+		nargv[0] = PS_BIN;
+	}
+
+	pipe(p);
+	
+	switch ((pid = fork())) {
+	case -1:
+		pdie("fork");
+	
+	case 0:
+		fd = open_read("/dev/null");
+		
+		dup2(fd,   0);
+		dup2(p[1], 1);
+		
+		close(p[0]);
+		close(p[1]);
+		close(fd);
+		
+		if (vx_migrate(1, NULL) == -1)
+			pdie("vx_migrate");
+		
+		if (fxid >= 0) {
+			if (execvp(nargv[0], nargv) == -1)
+				pdie("execvp");
+		} else {
+			if (execvp(argv[0], argv) == -1)
+				pdie("execvp");
+		}
+	
+	default:
+		close(p[1]);
+		
+		for (i = 0; ; i++) {
+			if ((len = io_read_eol(p[0], &line)) == -1)
+				pdie("io_read_eol");
+			
+			if (!len)
+				break;
+			
+			if (error_mode)
+				printf("%s\n", line);
+			else
+				parse_line(line, i);
+		}
+		
+		close(p[0]);
+		
+		if (waitpid(pid, &status, 0) == -1)
+			pdie("waitpid");
+	}
+	
+	exit(EXIT_SUCCESS);
+}
---
+++ src/wrapper.h
@@ -0,0 +1,182 @@
+// Copyright 2006 Benedikt Böhm <hollow@gentoo.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef _WRAPPER_H
+#define _WRAPPER_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <vserver.h>
+#include <sys/wait.h>
+#include <lucid/chroot.h>
+#include <lucid/io.h>
+#include <lucid/open.h>
+
+static char vdir[PATH_MAX];
+
+static
+void die(const char *msg, ...)
+{
+	va_list ap;
+	va_start(ap, msg);
+	
+	vdprintf(STDERR_FILENO, msg, ap);
+	dprintf(STDERR_FILENO, "\n");
+	
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+static
+void pdie(const char *msg, ...)
+{
+	char *errstr = strerror(errno);
+	
+	va_list ap;
+	va_start(ap, msg);
+	
+	vdprintf(STDERR_FILENO, msg, ap);
+	dprintf(STDERR_FILENO, ": %s\n", errstr);
+	
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+static
+void lookup_vdir(xid_t xid)
+{
+	int p[2], fd, status;
+	pid_t pid;
+	char procroot[PATH_MAX], buf[PATH_MAX], *data;
+	struct vx_info info;
+	
+	pipe(p);
+	
+	switch ((pid = fork())) {
+	case -1:
+		pdie("fork");
+	
+	case 0:
+		fd = open_read("/dev/null");
+		
+		dup2(fd,   0);
+		dup2(p[1], 1);
+		
+		close(p[0]);
+		close(p[1]);
+		close(fd);
+		
+		if (vx_get_info(xid, &info) == -1)
+			pdie("vx_get_info");
+		
+		/* TODO: recurse through process list and find one with xid */
+		if (info.initpid < 2)
+			die("invalid initpid");
+		
+		snprintf(procroot, PATH_MAX, "/proc/%d/root", info.initpid);
+		
+		if (vx_migrate(1, NULL) == -1)
+			pdie("vx_migrate");
+		
+		bzero(buf, PATH_MAX);
+		
+		if (readlink(procroot, buf, PATH_MAX - 1) == -1)
+			pdie("readlink");
+		
+		printf(buf);
+		
+		exit(EXIT_SUCCESS);
+	
+	default:
+		close(p[1]);
+		io_read_eof(p[0], &data);
+		close(p[0]);
+		
+		bzero(vdir, PATH_MAX);
+		strncpy(vdir, data, PATH_MAX - 1);
+		free(data);
+		
+		if (waitpid(pid, &status, 0) == -1)
+			pdie("waitpid");
+		
+		if (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS)
+			exit(EXIT_FAILURE);
+		
+		if (WIFSIGNALED(status)) {
+			dprintf(STDERR_FILENO, "caught signal %d", WTERMSIG(status));
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+int default_wrapper(int argc, char **argv, char *proc, int needxid)
+{
+	xid_t xid;
+	
+	/* check for xid and shuffle arguments */
+	if (argc > 2 && strcmp(argv[1], "--xid") == 0) {
+		xid = atoi(argv[2]);
+		argv[2] = proc;
+		argv = &argv[2];
+		argc -= 2;
+	}
+	
+	else {
+		xid = 1;
+		argv[0] = proc;
+	}
+	
+	if (needxid && xid < 2)
+		die("xid must be between 2 and 65535");
+	
+	if (xid > 1) {
+		lookup_vdir(xid);
+		
+		if (vx_enter_namespace(xid) == -1)
+			pdie("vx_enter_namespace");
+		
+		if (chroot_secure_chdir(vdir, "/") == -1)
+			pdie("chroot_secure_chdir");
+		
+		if (chroot(".") == -1)
+			pdie("chroot");
+	}
+	
+	if (vx_migrate(xid, NULL) == -1)
+		pdie("vx_migrate");
+	
+	if (execvp(argv[0], argv) == -1)
+		pdie("execvp");
+	
+	return EXIT_FAILURE;
+}
+
+#define DEFAULT_WRAPPER(PROC, NEEDXID) \
+int main(int argc, char **argv) { \
+	return default_wrapper(argc, argv, PROC, NEEDXID); \
+}
+
+#endif
